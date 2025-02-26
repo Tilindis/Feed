bd45941205830dd4c34d64f374f52ae5f75a76af
D. Tilindis <heronightmares@gmail.com>
2025-02-26 21:29:04 +0200

    Add logic to pass data from interactor to feeds view

---
+++ app/src/main/java/com/ito/feed/feature/feed/FeedsContent.kt
@@ -0,0 +1,62 @@
+package com.ito.feed.feature.feed
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.ito.feed.R
+import com.ito.feed.utils.model.FeedsModel
+import com.ito.feed.utils.ui.QueryCard
+
+@Composable
+fun FeedsContent(
+    feeds: List<FeedsModel>,
+    parameterInputs: List<String>,
+    onFieldSelected: (Int) -> Unit,
+    onFieldValueChange: (String) -> Unit,
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_logo_github_icon),
+            contentDescription = null,
+            modifier = Modifier
+                .size(136.dp)
+                .padding(16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .weight(weight = 1f, fill = false)
+        ) {
+            feeds.forEach { feed ->
+                QueryCard(
+                    path = feed.path,
+                    pathParameters = feed.parameters,
+                    parameterInputs = parameterInputs,
+                    onFieldSelected = onFieldSelected,
+                    onFieldValueChange = onFieldValueChange
+                )
+            }
+        }
+    }
+}
