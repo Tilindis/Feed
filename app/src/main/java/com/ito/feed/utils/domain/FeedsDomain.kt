a9a822fe18ab836e3b98781ccf0cd331d86b69bf
D. Tilindis <heronightmares@gmail.com>
2025-02-27 21:44:53 +0200

    Add card id to track cards

--- app/src/main/java/com/ito/feed/utils/domain/FeedsDomain.kt
+++ app/src/main/java/com/ito/feed/utils/domain/FeedsDomain.kt
@@ -5,12 +5,14 @@ import com.ito.feed.utils.model.FeedsModel
 data class FeedsDomain(
     val url: String,
     val path: String,
+    val cardId: Int,
     val parameters: List<Parameter>,
 ) {
     fun toFeedsModel(): FeedsModel {
         return FeedsModel(
             url = url,
             path = path,
+            cardId = cardId,
             parameters = parameters,
         )
     }
@@ -18,5 +20,6 @@ data class FeedsDomain(
 
 data class Parameter(
     val name: String,
+    val cardId: Int,
     val fieldNumber: Int,
 )
